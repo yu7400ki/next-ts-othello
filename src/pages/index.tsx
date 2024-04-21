2e8a925b99051476e80b11ecce74c5630e0744de
yu7400ki <yu7400ki@gmail.com>
2024-04-22 01:56:17 +0900

    save to query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styles from './index.module.css';
 import {
   type Board,
@@ -11,6 +10,7 @@ import {
   status,
   Status,
 } from '../othello';
+import { useRouter } from 'next/router';
 
 const Legal = 3 as const;
 type Legal = typeof Legal;
@@ -32,19 +32,37 @@ const intoArray = (board: Board, legalMoves: bigint): (Stone | Legal | null)[] =
   return result;
 };
 
+const toAlphabet = (i: number): string =>
+  String.fromCharCode('a'.charCodeAt(0) + (i % 8)) + (Math.floor(i / 8) + 1);
+const fromAlphabet = (s: string): number =>
+  s.charCodeAt(0) - 'a'.charCodeAt(0) + (parseInt(s[1]) - 1) * 8;
+const fromRecord = (record: string): [Board, Stone] => {
+  let board: Board = initialBoard;
+  let turn: Stone = Stone.Black;
+  if (!record.match(/^([a-h][1-8]){0,64}$/)) throw new Error();
+  for (const pos of record.match(/[a-h][1-8]/g) ?? []) {
+    const mask = 1n << BigInt(fromAlphabet(pos));
+    if (!isLegalMove(board, turn, mask)) throw new Error();
+    board = move(board, turn, mask);
+    if (makeLegalMoves(board, opposite(turn))) {
+      turn = opposite(turn);
+    }
+  }
+  return [board, turn];
+};
+
 const Home = () => {
-  const [board, setBoard] = useState<Board>(initialBoard);
-  const [turn, setTurn] = useState<Stone>(Stone.Black);
+  const router = useRouter();
+  const record = router.query.record ?? '';
+  if (typeof record !== 'string') throw new Error();
+  const [board, turn] = fromRecord(record);
   const legalMoves = makeLegalMoves(board, turn);
 
   const handleClick = (i: number) => () => {
     const pos = 1n << BigInt(i);
     if (!isLegalMove(board, turn, pos)) return;
-    const newBoard = move(board, turn, pos);
-    setBoard(newBoard);
-    if (makeLegalMoves(newBoard, opposite(turn))) {
-      setTurn(opposite(turn));
-    }
+    const nextRecord = record + toAlphabet(i);
+    router.push(`/?record=${nextRecord}`);
   };
 
   return (
